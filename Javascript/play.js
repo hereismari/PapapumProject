10fe69d8a93b9e6024612ed30eb8907b093cbc48
mari-linhares <marianne.monteiro@ccc.ufcg.edu.br>
2015-07-16 21:20:25 -0300

    Arranged basic folders

--- Javascript/play.js
+++ Javascript/play.js
@@ -22,14 +22,14 @@ $(document).ready(function(){
 
   $('div').on('click', function() {
 
-    document.body.style.backgroundImage = "url('Design/gif.gif')";
-    $('body').append('<embed src="/home/dell/FindIggy/iggysounds/iggy_was_found.mp3" autostart="true" hidden="true" loop="false">');
-    $('body').css('background-image', 'Design/gif.gif');
+    document.body.style.backgroundImage = "url('Resources/Image/gif.gif')";
+    $('body').append('<embed src="/home/dell/PapapumProject/Resources/Sound/iggy_was_found.mp3" autostart="true" hidden="true" loop="false">');
+    $('body').css('background-image', 'Resources/Image/gif.gif');
     $(this).animate({left: (($(document).width())/2).toFixed(),
             top: (($(document).height())/2).toFixed(),
             height: '150px',
             width: '150px'},"slow");
-    //$('body').append('<iframe src="http://gifyoutube.com/embed/vq65Yj?autoplay=1" frameborder="0" scrolling="no" width="480" height="270" autostart="true" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>');
+            //$('body').append('<iframe src="http://gifyoutube.com/embed/vq65Yj?autoplay=1" frameborder="0" scrolling="no" width="480" height="270" autostart="true" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>');
   });
 
 
