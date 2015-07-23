dd5467c28d60067811a23c31e07e94be8375c39c
mari-linhares <marianne.monteiro@ccc.ufcg.edu.br>
2015-07-23 14:04:28 -0300

    Changes for better view

--- Javascript/play.js
+++ Javascript/play.js
@@ -1,37 +1,66 @@
+var posx;
+var posy;
+
+var on;
+
 $(document).ready(function(){
 
-  (function makeDiv(){
+    on = true;
 
-        var posx = (Math.random() * ($(document).width() - 10)).toFixed();
-        var posy = (Math.random() * ($(document).height() - 10)).toFixed();
+    posx = (Math.random() * ($(document).width() - 10)).toFixed();
+    posy = (Math.random() * ($(document).height() - 10)).toFixed();
 
-        $newdiv = $('<div/>').css({
-            'width': 10 +'px',
-            'height': 10 +'px',
-            'background-color': '#666',
+    (function makeDiv(){
+
+        $newdiv = $('<div id="papapum" style="cursor:pointer" />').css({
+            'width': 50 +'px',
+            'height': 50 +'px',
+            'background-color': '#ffffff',
             'position':'absolute',
             'left':posx+'px',
             'top':posy+'px',
             'display':'none'
         });
         
-        $newdiv.appendTo( 'body' ).fadeIn(100); 
+        $newdiv.appendTo('body').fadeIn(100); 
 
     })();
 
+  $('#papapum').on('click', function() {
 
-  $('div').on('click', function() {
-
-    document.body.style.backgroundImage = "url('Resources/Image/gif.gif')";
-    $('body').append('<embed src="/home/dell/PapapumProject/Resources/Sound/iggy_was_found.mp3" autostart="true" hidden="true" loop="false">');
-    $('body').css('background-image', 'Resources/Image/gif.gif');
-    $(this).animate({left: (($(document).width())/2).toFixed(),
-            top: (($(document).height())/2).toFixed(),
-            height: '150px',
-            width: '150px'},"slow");
-            //$('body').append('<iframe src="http://gifyoutube.com/embed/vq65Yj?autoplay=1" frameborder="0" scrolling="no" width="480" height="270" autostart="true" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>');
+    $("game_song").volume = 0;
+    $(this).animate({
+            left: (($(document).height())/4).toFixed(),
+            top: (($(document).height())/20).toFixed(),
+            height: '400px',
+            width: '900px'
+        },"slow");
+            $('#papapum').append('<video autoplay width=900 height=400> <source src="Resources/Video/papapum.mp4" type="video/mp4">Your browser does not support the video tag.</video>');
   });
 
-
-
 });
+
+(function() {
+    document.onmousemove = setSound;
+    function setSound(event) {
+        //if(on == false){
+            var e = window.event;
+            var mousex = e.clientX;
+            var mousey = e.clientY;
+            var dist = Math.sqrt((posx - mousex) * (posx - mousex) + (posy - mousey)*(posy - mousey));
+            var volumeSong = 1 - Math.pow(2,Math.log(dist + 10)/Math.LN10)/10.0;
+          //  document.write(volumeSong);
+            var song = document.getElementById('game_song');
+            song.volume = volumeSong;
+
+         //   if(dist >= 750 &&  song.src != "file:///home/dell/PapapumProject/Resources/Music/calmFree.mp3"){
+         //       song.src="Resources/Sound/calmFree.mp3";
+         //   }
+         //   else if (dist >= 300){
+         //        song.src="Resources/Sound/when.mp3";
+         //   }
+         //   else
+         //      song.src="Resources/Sound/live.mp3";
+
+    }
+})();
